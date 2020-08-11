b4f238313b9352c2ee16d4c5f99a178ac025379e
jaimegsandoval <jaime.devs@gmail.com>
2020-08-11 16:50:47 -0700

    Changed the output for handleGradesSuccess

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var apiKey = "S0q6EVR2";
+var table = document.querySelector('table');
 
-var app = new App();
+var gradeTable = new GradeTable(table);
+var app = new App(gradeTable);
 
 app.start();
