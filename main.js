2a0c5476758ff686397a603beb2641335200dac9
jaimegsandoval <jaime.devs@gmail.com>
2020-08-12 12:11:55 -0700

    Added the grade-form class

--- main.js
+++ main.js
@@ -2,9 +2,11 @@
 var table = document.querySelector('table');
 
 
-var headerElement = document.querySelector('header');
-var pageHeader = new PageHeader(headerElement);
-var gradeTable = new GradeTable(table);
-var app = new App(gradeTable, pageHeader);
+var headerEl = document.querySelector('header');
+var formEl = document.querySelector('form');
+var pageHead = new PageHeader(headerEl);
+var gradeTble = new GradeTable(table);
+var gradeFormInput = new GradeForm(formEl)
+var app = new App(gradeTble, pageHead, gradeFormInput);
 
 app.start();
