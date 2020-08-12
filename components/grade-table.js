ff07c0d92f12b3cc01e9a67bff24a9f0a77be9bf
jaimegsandoval <jaime.devs@gmail.com>
2020-08-11 17:21:17 -0700

    Added the append rows functionality. Too many students

--- components/grade-table.js
+++ components/grade-table.js
@@ -4,6 +4,25 @@ class GradeTable {
   }
 
   updateGrades(grades) {
-    console.log(grades);
+    var tableBody = document.querySelector('tbody');
+    while (tableBody.firstChild) {
+      tableBody.removeChild(tableBody.firstChild);
+    }
+
+
+    console.log(grades)
+
+    for (var i = 0; i < grades.length; i++) {
+      var tr = document.createElement('tr');
+      var name = document.createElement('td');
+      var course = document.createElement('td');
+      var grade = document.createElement('td');
+      name.textContent = grades[i].name;
+      course.textContent = grades[i].course;
+      grade.textContent = grades[i].grade;
+      tr.append(name, course, grade);
+      tableBody.appendChild(tr);
+    }
+
   }
 }
